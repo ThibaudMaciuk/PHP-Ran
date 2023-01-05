e37ca395e63848a800c6e266f69e98a9f992c2d7
Thibaud Maciuk <thibaud.maciuk@le-campus-numerique.fr>
2023-01-05 11:40:58 +0100

    Iteration4-Partie1.4

--- poo/Humain.php
+++ poo/Humain.php
@@ -16,15 +16,32 @@ class Humain
     {
         echo ($this->taille+1);
     }
+
+    public function __construct()
+    {
+        echo "Je suis né.e";
+    }
+    public function __destruct()
+    {
+        echo "Je suis mort.e";
+    }
+
 }
 $marcelline = new Humain();
-
+echo "\r\n";
 $constance = new Humain();
-
+echo "\r\n";
 
 echo $marcelline->marcher();
-echo $constance->taille;
-echo $marcelline->marcher();
-echo $constance->taille; 
-echo $marcelline->maTaille();
-echo $constance->maTaille(); 
+echo "\r\n";
+echo nl2br($marcelline->taille);
+echo "\r\n";
+echo nl2br($constance->marcher());
+echo "\r\n";
+echo nl2br($constance->taille);
+echo "\r\n"; 
+echo nl2br($marcelline->maTaille());
+echo "\r\n";
+echo nl2br($constance->maTaille()); 
+echo "\r\n";
+unset ($constance);
