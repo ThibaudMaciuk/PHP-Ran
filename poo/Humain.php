0944f6b1882cba1978a77acfa95db629473c2ab7
Thibaud Maciuk <thibaud.maciuk@le-campus-numerique.fr>
2023-01-05 11:21:18 +0100

    Iteration4-Partie1.4

--- poo/Humain.php
+++ poo/Humain.php
@@ -1,7 +1,30 @@
 <?php
 
-
 class Humain
 {
-    
+    public $taille = 172;
+
+    public $nom;
+
+
+    public function marcher()
+    {
+        echo "Je marche";
+    }
+
+    public function maTaille()
+    {
+        echo ($this->taille+1);
+    }
 }
+$marcelline = new Humain();
+
+$constance = new Humain();
+
+
+echo $marcelline->marcher();
+echo $constance->taille;
+echo $marcelline->marcher();
+echo $constance->taille; 
+echo $marcelline->maTaille();
+echo $constance->maTaille(); 
