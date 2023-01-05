70fed95bb92aeaeb1ea3dc6e6b328c15f9fe23cf
Thibaud Maciuk <thibaud.maciuk@le-campus-numerique.fr>
2023-01-05 11:49:20 +0100

    temp

--- poo/Humain.php
+++ poo/Humain.php
@@ -3,33 +3,28 @@
 class Humain
 {
     public $taille = 172;
-
     public $nom;
-
-
     public function marcher()
     {
         echo "Je marche";
     }
-
     public function maTaille()
     {
         echo ($this->taille+1);
     }
-
-    public function __construct()
+    public function __construct(string $nomDeFamille)
     {
         echo "Je suis né.e";
+        $this->nom = $nom." ".$nomDeFamille;
     }
     public function __destruct()
     {
         echo "Je suis mort.e";
     }
-
 }
-$marcelline = new Humain();
+$marcelline = new Humain(randomeNom(10));
 echo "\r\n";
-$constance = new Humain();
+$constance = new Humain(randomeNom(10));
 echo "\r\n";
 
 echo $marcelline->marcher();
