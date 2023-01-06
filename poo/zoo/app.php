90e7af18060c59f900fba6b430f186fccb37b16e
Thibaud Maciuk <thibaud.maciuk@le-campus-numerique.fr>
2023-01-06 13:48:15 +0100

    Etape2->Fini

--- poo/zoo/app.php
+++ poo/zoo/app.php
@@ -3,27 +3,30 @@ namespace App;
 require __DIR__ . '/vendor/autoload.php';
 
 // Here comes your code.*
-function add_to_array(int $x, string $y, string $name_class, array $array)
+$array = [];
+function add_to_array(int $x, string $y, string $name_class)
 {
+    global $array;
     for ($i=0; $i<$x; $i++)
     {
-        $name = $y . $x;
+        $name = $y . $i;
+        //echo $name." Est mis dans le tableau";
         $o = new $name_class($name);
         $array[$name] = $o->noise();
+        //var_dump($array);
         //echo $o->noise();
     }
 }
 
-$array = [];
-add_to_array(10, 'Parrot', Parrot::class, $array);
-add_to_array(5, 'poisson', Fish::class, $array);
-add_to_array(3, 'poissonBulle', BubbleFish::class, $array);
-add_to_array(2, 'poissonChat', CatFish::class, $array);
-add_to_array(1, 'poissonClown', ClownFish::class, $array);
-add_to_array(2, 'Elephant', Elephant::class, $array);
-add_to_array(1, 'Zebre', Zebra::class, $array);
-add_to_array(2, 'Dove', Dove::class, $array);
-var_dump($array);
+add_to_array(10, 'Parrot', Parrot::class);
+add_to_array(5, 'poisson', Fish::class);
+add_to_array(3, 'poissonBulle', BubbleFish::class);
+add_to_array(2, 'poissonChat', CatFish::class);
+add_to_array(1, 'poissonClown', ClownFish::class);
+add_to_array(2, 'Elephant', Elephant::class);
+add_to_array(1, 'Zebre', Zebra::class);
+add_to_array(2, 'Dove', Dove::class);
+//var_dump($array);
 foreach ($array as $key=>$animal)
 {
     echo $key . '->' . $animal .PHP_EOL;
