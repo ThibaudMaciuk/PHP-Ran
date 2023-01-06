51943bf61853090833eafeca7087b2fbc259acfc
Thibaud Maciuk <thibaud.maciuk@le-campus-numerique.fr>
2023-01-06 09:21:23 +0100

    Pending zoo

--- poo/zoo/app.php
+++ poo/zoo/app.php
@@ -2,20 +2,41 @@
 require __DIR__ . '/vendor/autoload.php';
 
 // Here comes your code.*
+use App\Fish;
+use App\BubbleFish;
+use App\CatFish;
+use App\ClownFish;
+use App\Elephant;
+use App\Zebra;
+use App\Parrot;
+use App\Dove;
+$array[] = array(); 
+$poisson1 = new Fish();
+$poisson2 = new Fish();
+$poisson3 = new Fish();
+$poisson4 = new Fish();
+$poisson5 = new Fish();
+$poissonBulle1 = new BubbleFish();
+$poissonBulle2 = new BubbleFish();
+$poissonBulle3 = new BubbleFish();
+$poissonChat1 = new CatFish();
+$poissonChat2 = new CatFish();
+$poissonClown1 = new ClownFish();
+$Elephant1 = new Elephant();
+$Elephant2 = new Elephant();
+$Zebra1 = new Zebra();
+$Parrot1 = new Parrot();
+$Parrot2 = new Parrot();
+$Parrot3 = new Parrot();
+$Parrot4 = new Parrot();
+$Parrot5 = new Parrot();
+$Parrot6 = new Parrot();
+$Parrot7 = new Parrot();
+$Parrot8 = new Parrot();
+$Parrot9 = new Parrot();
+$Parrot10 = new Parrot();
+$Dove1 = new Dove();
+$Dove2 = new Dove();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+array_push($array, $poisson1, $poisson2, $poisson3, $poisson4, $poisson5, $poissonBulle1, $poissonBulle2, $poissonBulle3, $poissonChat1, $poissonChat2, $poissonClown1, $Elephant1, $Elephant2, $Zebra1, $Parrot1, $Parrot2, $Parrot3, $Parrot4, $Parrot5, $Parrot6, $Parrot7, $Parrot8, $Parrot9, $Parrot10, $Dove1, $Dove2);
+echo $array;
--- poo/zoo/src/Fish.php
+++ poo/zoo/src/Fish.php
@@ -1,5 +1,6 @@
 <?php
 
+use Animal;
 class Fish extends Animal
 {
     public function __construct($name)
