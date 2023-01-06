5b91cb6bc62b0495c25e1ab2d80d25fa34cdd457
Thibaud Maciuk <thibaud.maciuk@le-campus-numerique.fr>
2023-01-06 14:45:05 +0100

    Pending Zoo

---
+++ poo/zoo/src/Enclosure.php
@@ -0,0 +1,13 @@
+<?php
+Class enclosure
+{
+    public $animals = [];
+    public function addAnimal( $animal)
+    {
+        $this->animals[] = $animal;
+    }
+    public function toString()
+    {
+        
+    }
+}
