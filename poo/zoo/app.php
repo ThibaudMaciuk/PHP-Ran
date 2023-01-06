ca2d4162e024cf3f9f1f4c96c3dd7253459c864d
Thibaud Maciuk <thibaud.maciuk@le-campus-numerique.fr>
2023-01-06 11:01:17 +0100

    Pending Zoo

--- poo/zoo/app.php
+++ poo/zoo/app.php
@@ -12,15 +12,15 @@ function add_to_array(int $x, string $y, string $name_class, array $array)
         $temp = new $name_class($name);
         $array['$name'] = $temp->getNoise();
     }
-    return $array;
+    return array_values($array);
 }
 
 $array = [];
 echo add_to_array(5, 'poisson', 'Fish', $array);
-echo add_to_array(3, 'poissonBulle', 'BubbleFish',$array);
-echo add_to_array(2, 'poissonChat', 'CatFish',$array);
-echo add_to_array(1, 'poissonClown', 'ClownFish',$array);
-echo add_to_array(2, 'Elephant', 'Elephant',$array);
-echo add_to_array(1, 'Zebre', 'Zebra',$array);
-echo add_to_array(10, 'Parrot', 'Parrot',$array);
-echo add_to_array(2, 'Dove', 'Dove',$array);
+echo add_to_array(3, 'poissonBulle', 'BubbleFish', $array);
+echo add_to_array(2, 'poissonChat', 'CatFish', $array);
+echo add_to_array(1, 'poissonClown', 'ClownFish', $array);
+echo add_to_array(2, 'Elephant', 'Elephant', $array);
+echo add_to_array(1, 'Zebre', 'Zebra', $array);
+echo add_to_array(10, 'Parrot', 'Parrot', $array);
+echo add_to_array(2, 'Dove', 'Dove', $array);
