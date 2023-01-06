9ae469badfbdc932457c49a9981d3425c71c08ff
Thibaud Maciuk <thibaud.maciuk@le-campus-numerique.fr>
2023-01-06 10:29:14 +0100

    Pending Zoo

--- poo/zoo/app.php
+++ poo/zoo/app.php
@@ -3,35 +3,24 @@ namespace App;
 require __DIR__ . '/vendor/autoload.php';
 
 // Here comes your code.*
-
+function add_to_array(int $x, string $y, string $name_class)
+{
+    for ($i=1; $i==$x; $i++)
+    {
+        $name = $y;
+        $name = $y . $i;
+        $temp = new $name_class($name);
+        $array['$name'] = $temp->getNoise();
+    }
+    echo $array;
+}
 
 $array[] = array(); 
-$poisson1 = new Fish();
-$poisson2 = new Fish();
-$poisson3 = new Fish();
-$poisson4 = new Fish();
-$poisson5 = new Fish();
-$poissonBulle1 = new BubbleFish();
-$poissonBulle2 = new BubbleFish();
-$poissonBulle3 = new BubbleFish();
-$poissonChat1 = new CatFish();
-$poissonChat2 = new CatFish();
-$poissonClown1 = new ClownFish();
-$Elephant1 = new Elephant();
-$Elephant2 = new Elephant();
-$Zebra1 = new Zebra();
-$Parrot1 = new Parrot();
-$Parrot2 = new Parrot();
-$Parrot3 = new Parrot();
-$Parrot4 = new Parrot();
-$Parrot5 = new Parrot();
-$Parrot6 = new Parrot();
-$Parrot7 = new Parrot();
-$Parrot8 = new Parrot();
-$Parrot9 = new Parrot();
-$Parrot10 = new Parrot();
-$Dove1 = new Dove();
-$Dove2 = new Dove();
-
-array_push($array, $poisson1, $poisson2, $poisson3, $poisson4, $poisson5, $poissonBulle1, $poissonBulle2, $poissonBulle3, $poissonChat1, $poissonChat2, $poissonClown1, $Elephant1, $Elephant2, $Zebra1, $Parrot1, $Parrot2, $Parrot3, $Parrot4, $Parrot5, $Parrot6, $Parrot7, $Parrot8, $Parrot9, $Parrot10, $Dove1, $Dove2);
-echo $array;
+add_to_array(5, poisson, Fish);
+add_to_array(3, poissonBulle, BubbleFish);
+add_to_array(2, poissonChat, CatFish);
+add_to_array(1, poissonClown, ClownFish);
+add_to_array(2, Elephant, Elephant);
+add_to_array(1, Zebre, Zebra);
+add_to_array(10, Parrot, Parrot);
+add_to_array(2, Dove, Dove);
